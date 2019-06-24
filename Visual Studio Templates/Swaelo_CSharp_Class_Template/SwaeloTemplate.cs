14021fb2e9f553711b245f1edda3559f54ae9ce2
swaelo <harleylaurie@rocketmail.com>
2019-06-24 16:45:08 +1000

    added author lines

---
+++ Visual Studio Templates/Swaelo_CSharp_Class_Template/SwaeloTemplate.cs
@@ -0,0 +1,16 @@
+﻿// ================================================================================================================================
+// File:        $safeitemname$.cs
+// Description: 
+// Author:	Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using System;
+using System.Collections.Generic;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+        
+    }
+}
